c86ca21ee198e2d8df03790a2f1e08786ffb54f0
Kazuma M <e175745@ie.u-ryukyu.ac.jp>
2018-01-13 20:22:58 +0900

    松本一馬

--- BlackJack-master-2/src/main/java/jp/ac/uryukyu/ie/e175745/Rule.java
+++ BlackJack-master-2/src/main/java/jp/ac/uryukyu/ie/e175745/Rule.java
@@ -13,6 +13,7 @@ public class Rule{
         String[] deck = cards.toArray(new String[cards.size()]);
         return deck;
     }
+
     public void deal(String[] deck) {
         for(int i=0;i<2;i++){
             p1.Player(deck[i]);
@@ -21,17 +22,32 @@ public class Rule{
             deck = cards.toArray(new String[cards.size()]);
         }
     }
+
     public int Total(int card){
-            int total=0;
-            total+= card;
-            return total;
-    }
-    public void Judge(int p_total,int d_total){
-        if (p_total > d_total){
-            System.out.println("you win!\nAs luck would have it.");
-        }else{
-            System.out.println("you lose,,\nIs there also such a day.");
+        int total;
+        ace = new ace();
+        if (card==11 || card==12 || card==13){
+            card =10;
+        }else if(card==1){
+            if(ace == true){
+                card = 10;
+            }
         }
+        total+= card;
+        return total;
     }
 
+    public void Judge(int p_total,int d_total){
+        int p_diff = 0;
+        int d_diff = 0;
+        p_diff = 21-p_total;
+        d_diff = 21-d_total;
+        if (d_diff > p_diff){
+            System.out.println("you win!\nAs luck would have it.");
+        }else if (p_diff > p_diff){
+            System.out.println("you lose,,\nIs there also such a day.");
+        }else{
+            System.out.println("draw\nIt was a good game.");
+        }
+    }
 }
