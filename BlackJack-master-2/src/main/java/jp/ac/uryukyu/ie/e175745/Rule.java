8bc04377713a3750ba7e973edbe20e81c1aac538
Kazuma M <e175745@ie.u-ryukyu.ac.jp>
2018-01-13 18:12:36 +0900

    松本一馬

---
+++ BlackJack-master-2/src/main/java/jp/ac/uryukyu/ie/e175745/Rule.java
@@ -0,0 +1,37 @@
+package jp.ac.uryukyu.ie.e175745;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+public class Rule{
+    Player p1 =new Player();
+
+    public String[] Shuffle(String[] card) {
+        List<String> cards = new LinkedList<>(Arrays.asList(card));
+        Collections.shuffle(cards);
+        String[] deck = cards.toArray(new String[cards.size()]);
+        return deck;
+    }
+    public void deal(String[] deck) {
+        for(int i=0;i<2;i++){
+            p1.Player(deck[i]);
+            List<String> cards = new LinkedList<>(Arrays.asList(deck));
+            cards.remove(i);
+            deck = cards.toArray(new String[cards.size()]);
+        }
+    }
+    public int Total(int card){
+            int total;
+            total+= card;
+            return total;
+    }
+    public void Judge(int p_total,int d_total){
+        if (p_total > d_total){
+            System.out.println("you win!\nAs luck would have it.");
+        }else{
+            System.out.println("you lose,,\nIs there also such a day.");
+        }
+    }
+
+}
